dd3482c9fcdd39a2ce54eb7bd40321445c6a759b
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-07 11:21:32 +0530

    code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+ import reactLogo from './assets/react.svg'
+ import viteLogo from '/vite.svg'
+import './App.css'
+import TicTacToe from './component/TicTacToe/TicTacToe'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TicTacToe/>
+    </>
+
+  )
+}
+
+export default App
